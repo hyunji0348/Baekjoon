22eec12bbcc373c4b8e92ee66848cf7e680645be
hyunji0348 <148692504+hyunji0348@users.noreply.github.com>
2024-02-12 18:56:56 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 1300 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.c
+++ 백준/Bronze/2745. 진법 변환/진법 변환.c
@@ -8,32 +8,23 @@ int main() {
 	int B;			// 진법
 	char N[100];	// 10진법으로 바꿀 수
 	int sum = 0;
-	int alphanum;	// 알파벳을 숫자로 치환해서 저장할 변수
+	int alphanum;	// 문자를 숫자로 치환해서 저장할 변수
 	int len;
 
 	scanf("%s %d", N, &B);
 	len = strlen(N);
 
 	for (int i = 0; i < len; i++) {
-		if(len == i-1) {	// 1의 자리의 수
-			if ('A' < N[i] && 'Z' > N[i]) { // A~Z
-				alphanum = N[i] - 55;
-				sum += alphanum;
-			}
-			else	// 0~9
-				sum += atoi(N[i]);
-		}
 
-		else {		// 1이 아닌 자리의 수
 			if (('A' <= N[i]) && ('Z' >= N[i])) { // A~Z
-				alphanum = N[i] - 55;
+				alphanum = N[i] - '7';
 				sum += alphanum * pow(B,(len - i - 1));
 			}
 			else {	// 0~9
-				alphanum = N[i] - 48;
+				alphanum = N[i] - '0';
 				sum += alphanum * pow(B, (len - i - 1));
 			}
-		}
+		
 	}
 
 	printf("%d", sum);
