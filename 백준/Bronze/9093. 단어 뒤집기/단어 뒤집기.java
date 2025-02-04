cf585330cfb77caaab49040030f1c5607e7579a8
hyunji0348 <148692504+hyunji0348@users.noreply.github.com>
2025-02-04 23:12:29 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 1172 ms, Memory: 61060 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++){
+            String[] words = br.readLine().split(" ");
+
+            for(String word : words){
+                StringBuilder sb = new StringBuilder(word);
+                System.out.print(sb.reverse() + " ");
+            }
+            System.out.println();
+        }
+        br.close();
+    }
+}
