80a976ea5eb661cf70d95ea1a0cc30f68320d8e5
김현지 <148692504+hyunji0348@users.noreply.github.com>
2026-02-18 17:15:20 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,12 @@
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        start = command[0]-1 
+        end = command[1]
+        new_list = array[start:end]
+        new_list.sort()
+        
+        answer.append(new_list[command[2]-1])
+        
+    return answer
