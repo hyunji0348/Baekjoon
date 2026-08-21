7f6615f7043c8fa587bb9fe6e21eadb48a145a05
김현지 <148692504+hyunji0348@users.noreply.github.com>
2026-08-21 16:35:36 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,12 +1,12 @@
 def solution(array, commands):
     answer = []
-    # for문 commands 순환
-        # 슬라이싱
-        # 정렬
-        # 해당 위치 찾아서 answer에 append 
-        
+    
     for command in commands:
-        sliced_array = array[command[0]-1:command[1]]
-        sliced_array.sort()
-        answer.append(sliced_array[command[2]-1])
+        start = command[0]-1 
+        end = command[1]
+        new_list = array[start:end]
+        new_list.sort()
+        
+        answer.append(new_list[command[2]-1])
+        
     return answer
