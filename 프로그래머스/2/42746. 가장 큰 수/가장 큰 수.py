50c628bc3a1c50803c654dd928cd9e8be521a6d2
김현지 <148692504+hyunji0348@users.noreply.github.com>
2026-08-21 17:43:04 +0900

    [level 2] Title: 가장 큰 수, Time: 32.52 ms, Memory: 26.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,15 +1,10 @@
 def solution(numbers):
-    answer = ''
-    
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x*3, reverse=True)
     
     answer = "".join(numbers)
     
-    if int(answer) <= 0:
-        answer = "0"
+    if answer[0] == '0':   # 문자열 그대로 확인, 변환 없음
+        answer = '0'
     
     return answer
-
-# 문자열 리스트 정렬 기준 특성 -> 문자열 원소 길이에 상관없이 무조건 앞자리부터 하나씩 크기비교
-# key값(lambda함수를 적용한 값)을 기준으로 정렬(실제 원소는 변형X)
