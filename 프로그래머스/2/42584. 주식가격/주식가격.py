03d0da25086d896635c8bfababfb27086c743cfb
김현지 <148692504+hyunji0348@users.noreply.github.com>
2026-08-21 16:08:31 +0900

    [level 2] Title: 주식가격, Time: 26.20 ms, Memory: 17 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,28 @@
+def solution(prices):
+    answer = [0] * len(prices)
+    stack = []
+    
+    # for문 순회
+        # if 비어있거나 새로운 값이 peek 값보다 같거나 크면:
+        # else: # 작은애 들어와서(값 하락) 스택 빼야함
+        #     while(peek의 값이 새로운애 값보다 클때):
+        #         answer[peek의 idx] = 새로운애 idx - peek의 idx 
+        #         스택에서 peek pop
+        #     새로운애 stack.append
+    
+    for idx, price in enumerate(prices): 
+        if not stack or price >= stack[-1][1]:
+            stack.append((idx, price))
+        else: # 작은애 들어와서(값 하락) 스택 빼야함
+            while(stack and price < stack[-1][1]):
+                answer[stack[-1][0]] = idx - stack[-1][0]
+                stack.pop()
+            stack.append((idx, price))
+            
+            
+    for idx, i in enumerate(answer):
+        if i == 0:
+            answer[idx] = (len(prices) - 1) - idx
+    
+    
+    return answer
